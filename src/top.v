cfdada56f4e8fe258c77dfb349edd8d48c11b724
Ryota Suzuki <ja1tye@gmail.com>
2023-09-17 12:27:24 +0900

    fix info.yaml

--- src/top.v
+++ src/top.v
@@ -15,18 +15,17 @@ module tt_um_ja1tye_sound_generator #( parameter MAX_COUNT = 24'd10_000_000 ) (
     reg [15:0] freq;
 
     wire [23:0] incr;
-    
+
     wire reset;
     assign reset = ~rst_n;
 
-    assign incr[23:0] = (freq[3:0] > 4'hc)?(freq[15:4] << 4'hc):(freq[15:4] << freq[3:0]);
+    assign incr[23:0] = {freq,8'h0};//(freq[3:0] > 4'hc)?(freq[15:4] << 4'hc):(freq[15:4] << freq[3:0]);
 
     assign uo_out = acc[23:16];
     // use bidirectionals as inputs
     assign uio_oe = 8'b00000000;
 
     always @(posedge clk) begin
-        // if reset, set counter to 0
         if (reset) begin
             acc <= 0;
             freq <= 0;
