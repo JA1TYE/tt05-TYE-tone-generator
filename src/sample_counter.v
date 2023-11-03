6a0dc45172da14d924cb188d8392d1289f114f2d
Ryota Suzuki <ja1tye@gmail.com>
2023-11-03 23:33:25 +0900

    add waveform select

--- src/sample_counter.v
+++ src/sample_counter.v
@@ -1,3 +1,4 @@
+`default_nettype none
 module sample_counter (
     input wire reset_in,
     input wire clk_in,
@@ -13,62 +14,76 @@ module sample_counter (
     reg [15:0] phase_acc[0:3];
     reg [15:0] phase_incr[0:3];
     reg [7:0]  volume[0:3];
+    reg [2:0]  wave_type[0:3];
     reg [15:0] mix_result;
+    reg sqr_buf[0:3];
 
     assign data_out = mix_result;
 
     wire [15:0] incr_out;
     assign incr_out = phase_incr[master_count_in[1:0]];
 
+    wire sqr_out;
+    wave_lut WAVE_LUT(.data_in(acc_out[15:13]),.wave_type_in(wave_type[master_count_in[1:0]]),.data_out(sqr_out));
+    
+    wire [15:0] dca_out;
+    assign dca_out = dca(sqr_buf[master_count_in[1:0]],volume[master_count_in[1:0]]);
+
     wire [15:0] a_in;
-    assign a_in = (master_count_in[2] == 1'b0)?incr_out:{acc_out[15],acc_out[15],acc_out[15:2]};
+    assign a_in = (master_count_in[3:2] == 2'b00)?incr_out:{ {2{dca_out[15]}},dca_out[15:2]};
 
     wire [15:0] acc_out;
     assign acc_out = phase_acc[master_count_in[1:0]];
 
     wire [15:0] b_in;
-    assign b_in = (master_count_in[2] == 1'b0)?acc_out:mix_result;
+    assign b_in = (master_count_in[3:2] == 2'b00)?acc_out:mix_result;
 
     reg sat_flag;
     wire [15:0]adder_out;
 
-    sat_adder adder(.a_in(a_in),.b_in(b_in),.c_out(adder_out),.sat_en_in(sat_flag));
+    sat_adder adder(.a_in(a_in),.b_in(b_in),.s_out(adder_out),.sat_en_in(sat_flag));
 
     always@(posedge clk_in)begin
         if(reset_in == 1'b1)begin
             data_valid_out <= 1'b0;
             sat_flag <= 1'b0;
             mix_result <= 16'h0;
+            
+            phase_acc[0] <= 16'h0;
+            phase_acc[1] <= 16'h0;
+            phase_acc[2] <= 16'h0;
+            phase_acc[3] <= 16'h0;
+            phase_incr[0] <= 16'h0;
+            phase_incr[1] <= 16'h0;
+            phase_incr[2] <= 16'h0;
+            phase_incr[3] <= 16'h0;
+            volume[0] <= 8'h0;
+            volume[1] <= 8'h0;
+            volume[2] <= 8'h0;
+            volume[3] <= 8'h0;
 
         end
         else begin
-            //Quick and Dirty...
-            if(master_count_in == 10'h0)begin
-                phase_acc[0] <= adder_out;
+            //Update Phase accumulator
+            if(master_count_in[9:2] == 8'h00)begin
+                phase_acc[master_count_in[1:0]] <= adder_out;
+            end
+            if(master_count_in[9:2] == 8'h01)begin
+                sqr_buf[master_count_in[1:0]] <= sqr_out;
+            end
+            //Mix each channels
+            if(master_count_in[9:2] == 8'h02)begin
+                mix_result <= adder_out;
+            end
+
+            //initialize mix_result and set sat_flag
+            if(master_count_in == 10'h3)begin
+                sat_flag <= 1'b1;
                 mix_result <= 16'h0;
             end
-            else if(master_count_in == 10'h1)begin
-                phase_acc[1] <= adder_out;
-            end
-            else if(master_count_in == 10'h2)begin
-                phase_acc[2] <= adder_out;
-            end
-            else if(master_count_in == 10'h3)begin
-                phase_acc[3] <= adder_out;
-                sat_flag <= 1'b1;
-            end
-            else if(master_count_in == 10'h4)begin
-                mix_result <= adder_out;
-            end
-            else if(master_count_in == 10'h5)begin
-                mix_result <= adder_out;
-            end
-            else if(master_count_in == 10'h6)begin
-                mix_result <= adder_out;
-            end
-            else if(master_count_in == 10'h7)begin
-                mix_result <= adder_out;
-                sat_flag <= 1'b0;
+
+            if(master_count_in == 10'hb)begin
+                sat_flag<= 1'b0;
                 data_valid_out <= 1'b1;
             end
             else begin
@@ -78,19 +93,107 @@ module sample_counter (
             if(data_valid_in == 1'b1)begin
                 if(addr_in[3:2] == 2'h0)begin
                     phase_incr[addr_in[1:0]] <= data_in[15:0];
-                end else if(addr_in[3:2] == 2'h1)begin
+                end
+                else if(addr_in[3:2] == 2'h1)begin
                     volume[addr_in[1:0]] <= data_in[7:0];
+                end
+                else if(addr_in[3:2] == 2'h2)begin
+                    wave_type[addr_in[1:0]] <= data_in[2:0];
                 end
             end
         end
     end
+
+    function [15:0]dca;
+        input   value_in;
+        input   [7:0] volume_in;
+        reg     [15:0]ext_volume;
+
+        dca = (value_in == 1'b1)?{1'b0,volume_in,volume_in[7:1]}:(~{1'b0,volume_in,volume_in[7:1]});
+        
+    endfunction
+
+endmodule
+
+module wave_lut(
+    input wire[2:0] data_in,
+    input wire[2:0] wave_type_in,
+    output wire data_out
+);
+    assign data_out = wave_lookup(data_in,wave_type_in);
+    function wave_lookup;
+        input [2:0] data_in;
+        input [2:0] wave_type_in;
+        if(wave_type_in == 3'h0)begin//0000 1111
+            wave_lookup = data_in[2];
+        end
+        else if(wave_type_in == 3'h1)begin//0000 0001
+            if(data_in == 3'h7)begin
+                wave_lookup = 1'b1;
+            end
+            else begin
+                wave_lookup = 1'b0;
+            end
+        end
+        else if(wave_type_in == 3'h2)begin//0000 0011
+            if(data_in == 3'h7 || data_in == 3'h6)begin
+                wave_lookup = 1'b1;
+            end
+            else begin
+                wave_lookup = 1'b0;
+            end
+        end
+        else if(wave_type_in == 3'h3)begin//0000 0111
+            if(data_in == 3'h7 || data_in == 3'h6 || data_in == 3'h5)begin
+                wave_lookup = 1'b1;
+            end
+            else begin
+                wave_lookup = 1'b0;
+            end
+        end
+        else if(wave_type_in == 3'h4)begin//0001 1111
+            if(data_in == 3'h0 || data_in == 3'h1 || data_in == 3'h2)begin
+                wave_lookup = 1'b0;
+            end
+            else begin
+                wave_lookup = 1'b1;
+            end
+        end
+        else if(wave_type_in == 3'h5)begin//0011 1111
+            if(data_in == 3'h0 || data_in == 3'h1)begin
+                wave_lookup = 1'b0;
+            end
+            else begin
+                wave_lookup = 1'b1;
+            end
+        end
+        else if(wave_type_in == 3'h6)begin//0111 1111
+            if(data_in == 3'h0)begin
+                wave_lookup = 1'b0;
+            end
+            else begin
+                wave_lookup = 1'b1;
+            end
+        end
+        else if(wave_type_in == 3'h7)begin//0000 1101
+            if(data_in == 3'h4 || data_in == 3'h5 || data_in == 3'h7)begin
+                wave_lookup = 1'b1;
+            end
+            else begin
+                wave_lookup = 1'b0;
+            end
+        end
+        else begin
+            wave_lookup = data_in[2];
+        end
+    endfunction
 
 endmodule
 
 module sat_adder(
     input   wire[15:0]  a_in,
     input   wire[15:0]  b_in,
-    output  wire[15:0]  c_out,
+    output  wire[15:0]  s_out,
     input   wire        sat_en_in
 );
     wire [15:0] result;
@@ -98,7 +201,7 @@ module sat_adder(
     
     assign result = a_in + b_in;
     assign signed_ovf = (a_in[15] == b_in[15]) && (a_in[15] != result[15]);
-    assign c_out = saturate(result,sat_en_in,signed_ovf); 
+    assign s_out = saturate(result,sat_en_in,signed_ovf); 
     
     function [15:0]saturate;
         input [15:0] value_in;
@@ -125,3 +228,4 @@ module sat_adder(
     endfunction
 
 endmodule
+
