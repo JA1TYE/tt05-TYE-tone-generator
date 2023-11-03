600d28122d6f0fe30bcae24814d8a59ca57cd91a
Ryota Suzuki <ja1tye@gmail.com>
2023-11-04 00:50:33 +0900

    change stereo to mono

--- src/tone_engine.v
+++ src/tone_engine.v
@@ -52,7 +52,7 @@ module tone_engine
     i2s_out I2S_TX(
             .reset_in(reset_in),.clk_in(clk_in),
             .master_count_in(master_count),
-            .left_in(sound_data),.right_in(sound_data),
+            .data_in(sound_data),
             .data_valid_in(sample_data_valid_out),
             .d_out(i2s_d_out),.ws_out(i2s_ws_out),.bclk_out(i2s_bclk_out)
     );
